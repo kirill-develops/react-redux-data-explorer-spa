ded84162bf366de715177088b1ba9777abdcff4b
Kirill Tchentsov <kirill.develops@gmail.com>
2026-05-11 19:06:13 -0400

    updated url for fetching

--- src/slices/universitySlice.js
+++ src/slices/universitySlice.js
@@ -1,47 +1,50 @@
 import { createEntityAdapter, nanoid } from "@reduxjs/toolkit";
 import { apiSlice } from "./apiSlice";
 
-
-const universityAdapter = createEntityAdapter({})
+const universityAdapter = createEntityAdapter({});
 
 const initialState = universityAdapter.getInitialState();
 
 export const extendedApiSlice = apiSlice.injectEndpoints({
-  endpoints: builder => ({
-    getCountries: builder.query({
-      query: () => ('https://countriesnow.space/api/v0.1/countries/info?returns=none'),
-      transformResponse: resData => resData.data.sort((a, b) => a.name.localeCompare(b.name))
-    }),
-    getUni: builder.query({
-      query: country => ({
-        url: `http://universities.hipolabs.com/search?country=${country}`,
+   endpoints: (builder) => ({
+      getCountries: builder.query({
+         query: () =>
+            "https://countriesnow.space/api/v0.1/countries/info?returns=none",
+         transformResponse: (resData) =>
+            resData.data.sort((a, b) => a.name.localeCompare(b.name)),
       }),
-      transformResponse: responseData => {
-        const resConvert = responseData.slice()
-          .sort((a, b) => a.name.localeCompare(b.name))
-          .map(each => {
-            return { ...each, id: nanoid() }
-          });
-        return universityAdapter.setAll(initialState, resConvert)
-      }
-    })
-  })
+      getUni: builder.query({
+         query: (country) => ({
+            url: `https://universities.hipolabs.com/search?country=${country}`,
+         }),
+         transformResponse: (responseData) => {
+            const resConvert = responseData
+               .slice()
+               .sort((a, b) => a.name.localeCompare(b.name))
+               .map((each) => {
+                  return { ...each, id: nanoid() };
+               });
+            return universityAdapter.setAll(initialState, resConvert);
+         },
+      }),
+   }),
 });
 
-export const {
-  useGetCountriesQuery,
-  useGetUniQuery } = extendedApiSlice;
+export const { useGetCountriesQuery, useGetUniQuery } = extendedApiSlice;
 
-export const { selectAll: getAllUniversity } = universityAdapter
-  .getSelectors(state => {
-    let newObj = {};
+export const { selectAll: getAllUniversity } = universityAdapter.getSelectors(
+   (state) => {
+      let newObj = {};
 
-    for (const value of Object.values(state.api.queries)) {
-      if (value?.endpointName === 'getUni'
-        && value?.status === 'fulfilled') {
-        newObj = value.data;
+      for (const value of Object.values(state.api.queries)) {
+         if (
+            value?.endpointName === "getUni" &&
+            value?.status === "fulfilled"
+         ) {
+            newObj = value.data;
+         }
       }
-    }
 
-    return !Object.values(newObj)[0] ? initialState : newObj;
-  })
+      return !Object.values(newObj)[0] ? initialState : newObj;
+   },
+);
